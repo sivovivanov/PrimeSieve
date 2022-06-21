11d50e4928620a5b5f169c507645d91be512f5c6
sivovivanov <sv.ivanov33@abv.bg>
2022-06-22 00:24:33 +0300

    Implemented prime sieve in C++ using vectors, future improvement might be to use arrays to increase speed further

--- cpp/source.cpp
+++ cpp/source.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
+#include <vector>
 
-int main(){
-    std::cout<<"Hello World!\n";
+using std::cout; using std::endl;
+using std::vector;
+
+vector<int> sieve(int n) {
+    vector<int> prime(n+1, 1);
+    int p = 2;
+    while(p * p <= n) {
+        if(prime[p]) {
+            for(int i = p * p; i < n+1; i+=p) {
+                prime[i] = 0;
+            }
+        }
+        p++;
+    }
+
+    return prime;
+}
+
+int main() {
+    int num = 100;
+    vector<int> prime = sieve(num);
+
+    for (int i = 2; i < num+1; i++) {
+        if(prime[i]) cout << i << endl;
+    }
+
     return 0;
 }
